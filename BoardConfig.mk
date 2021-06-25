01f98195b41f45171faa90773a9165597aa9a841
Stricted <info@stricted.net>
2021-06-25 01:24:03 -0400

    km9pro: Build device specific DTB
    Change-Id: I882a5f2481802cabe28204ee9c65905d1a80f984

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -9,9 +9,11 @@ DEVICE_PATH := device/videostrong/km9pro
 ## Bootloader
 TARGET_BOOTLOADER_BOARD_NAME := km9pro
 
+## DTB
+TARGET_DTB_NAME := g12a_s905x2_u212_sti6130d3x0
+
 ## Include the common tree BoardConfig makefile
 include device/amlogic/g12-common/BoardConfigCommon.mk
 
 ## Include the proprietary BoardConfig makefile
 include vendor/videostrong/km9pro/BoardConfigVendor.mk
-
