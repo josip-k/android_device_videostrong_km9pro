787a7d82301829d902629c2f8b3a6b801e776b86
Aaron Kling <webgeek1234@gmail.com>
2021-06-30 19:19:40 -0500

    km9pro: Configure Realtek Bluetooth/Wi-Fi HALs
    * We're not unifying things, and the stock MultiHAL requires
      framework side magic, so we're going with the hardware/realtek
      solution.
    
    * Set Bluetooth overlays based on stock.
    
    Change-Id: Ie8e0f2d2a5f1cb8337ebe88b903fb18a71602f18

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -18,3 +18,12 @@ include device/amlogic/g12-common/BoardConfigCommon.mk
 
 ## Include the proprietary BoardConfig makefile
 include vendor/videostrong/km9pro/BoardConfigVendor.mk
+
+## Wi-Fi
+BOARD_WLAN_DEVICE := realtek
+BOARD_WPA_SUPPLICANT_DRIVER := NL80211
+WPA_SUPPLICANT_VERSION := VER_0_8_X
+BOARD_HOSTAPD_PRIVATE_LIB := lib_driver_cmd_rtl
+BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_rtl
+WIFI_DRIVER_SOCKET_IFACE := wlan0
+PRODUCT_CFI_INCLUDE_PATHS += hardware/realtek/wlan/wpa_supplicant_8_lib
--- device.mk
+++ device.mk
@@ -10,6 +10,14 @@ $(call inherit-product, vendor/videostrong/km9pro/km9pro-vendor.mk)
 
 $(call inherit-product, device/amlogic/g12-common/g12.mk)
 
+## Bluetooth
+BOARD_HAVE_BLUETOOTH_RTK_TV := true
+include hardware/realtek/rtkbt/rtkbt.mk
+
 ## Kernel Modules
 PRODUCT_PACKAGES += \
     8821cs
+
+## Overlays
+DEVICE_PACKAGE_OVERLAYS += \
+    $(DEVICE_PATH)/overlay
