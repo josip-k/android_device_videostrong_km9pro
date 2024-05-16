1bfdfe03e2d2aa748b5066ef5f1f23862412a902
Josip Kelecic <kelecicj@gmail.com>
2024-05-16 22:51:47 +0200

    km9pro: Add Dolby properties

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -13,6 +13,9 @@ TARGET_BOOTLOADER_BOARD_NAME := km9pro
 TARGET_DTB_NAME := g12a_s905x2_u212_sti6130d3x0
 TARGET_DTBO_NAME := sti6140d350_overlay
 
+## Properties
+TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
+
 ## Include the common tree BoardConfig makefile
 include device/amlogic/g12-common/BoardConfigCommon.mk
 
